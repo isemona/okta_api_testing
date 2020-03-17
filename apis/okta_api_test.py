99a6f89b9842c3a68e3361101c4ee107bc81bd1f
Semona Igama <semona.igama@gmail.com>
2020-03-16 18:36:08 -0700

    adding code to activate a user and loop through list of users, refactored code

--- apis/okta_api_test.py
+++ apis/okta_api_test.py
@@ -1,53 +1,53 @@
-# import json
-# import requests
+"""Helper functions used to create users"""
 
 
-# api_token = '00l-98rUxuBYotnn-DfJF9kuI79PWMxonN7ItRnPOX'
-# api_url_base = 'https://semonav2.oktapreview.com/api/v1/users'
-
-
-# headers = {'Content-Type': 'application/json',
-#            'Authorization': 'Bearer {0}'.format(api_token)}
-
-
-# def get_account_info():
-
-#     api_url = '{0}users'.format(api_url_base)
-
-#     response = requests.get(api_url, headers=headers)
-
-#     if response.status_code == 200:
-#         return json.loads(response.content.decode('utf-8'))
-#     else:
-#         return None
-
-# account_info = get_account_info()
-
-# print(account_info)
-# if account_info is not None:
-#     print("Here's your info: ")
-#     for k, v in account_info['account'].items():
-#         print('{0}:{1}'.format(k, v))
-
-# else:
-#     print('[!] Request Failed')
-
-
-
+# Create a client
 from okta import UsersClient
 # http://developer.okta.com/docs/api/getting_started/getting_a_token.html
 usersClient = UsersClient('https://example.oktapreview.com/',
                           '01a2B3Cd4E5fGHiJ6K7l89mNOPQRsT0uVwXYZA1BCd')
 
-
+# Create a user
 from okta import UsersClient
 # http://developer.okta.com/docs/api/getting_started/getting_a_token.html
 usersClient = UsersClient('https://example.oktapreview.com/',
                           '01a2B3Cd4E5fGHiJ6K7l89mNOPQRsT0uVwXYZA1BCd')
 
-new_user = User(login='example@example.com',
-                email='example@example.com',
+new_user = User(login='saml.jackson@oktapreview.com',
+                email='saml.jackson@oktapreview.com',
                 firstName='Saml',
                 lastName='Jackson')
 user = usersClient.create_user(new_user, activate=False)
+
+# Activate a user
+from okta import UsersClient
+from okta.models.user import User
+# http://developer.okta.com/docs/api/getting_started/getting_a_token.html
+usersClient = UsersClient('https://example.oktapreview.com/',
+                          '01a2B3Cd4E5fGHiJ6K7l89mNOPQRsT0uVwXYZA1BCd')
+
+user = usersClient.get_user('example@example.com')
+usersClient.activate_user(user.id)
+
+# Loop through a list
+from okta import UsersClient
+# http://developer.okta.com/docs/api/getting_started/getting_a_token.html
+usersClient = UsersClient('https://example.oktapreview.com/',
+                          '01a2B3Cd4E5fGHiJ6K7l89mNOPQRsT0uVwXYZA1BCd')
+
+users = usersClient.get_paged_users()
+while True:
+    for user in users.result:
+        print u"First Name: {}".format(user.profile.firstName)
+        print u"Last Name:  {}".format(user.profile.lastName)
+        print u"Login:      {}".format(user.profile.login)
+        print u"User ID:    {}\n".format(user.id)
+    if not users.is_last_page():
+        # Keep on fetching pages of users until the last page
+        users = usersClient.get_paged_users(url=users.next_url)
+    else:
+        break
+
+
+
 
